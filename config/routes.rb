d2ff6fb2e93aa7d9c33aea3ad635a18aa6703ec7
Ami Mahloof <ami@Ami-Mahloofs-MacBook-Pro.local>
2009-10-18 21:23:45 -0400

    haim last polish

--- config/routes.rb
+++ config/routes.rb
@@ -8,13 +8,17 @@ ActionController::Routing::Routes.draw do |map|
   map.login 'login', :controller => "user_sessions", :action => "new"  
   map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'  
   
-  map.resources :users, :categories, :services, :appointments, :stylists, :products
+  
+  map.resources :users, :categories, :services, :appointments, :stylists, :products, :specials
   map.resource :user_session
   
   map.with_options :controller => 'info' do |info|
     info.about_us  'about_us',  :action => "about_us"
     info.specials 'specials',   :action => "specials"
     info.location 'location',   :action => "location"
+    info.keratin 'keratin', :action => "keratin"
+    info.appointments 'appointments', :action => "appointments"
+    info.contact_us 'contact_us', :action => "contact_us" 
   end
   
 end
