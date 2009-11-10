e58a2fefa19a259da26fcfb49eb4834d456037cf
Ami Mahloof <ami@Ami-Mahloofs-MacBook-Pro.local>
2009-11-10 00:37:39 -0500

    =services with levels

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,5 @@
 class Service < ActiveRecord::Base
-  belongs_to :stylist
   belongs_to :category
   has_many :specials
   validates_presence_of :name
-  
 end
--- db/migrate/20090525154441_create_services.rb
+++ db/migrate/20090525154441_create_services.rb
@@ -1,10 +1,14 @@
 class CreateServices < ActiveRecord::Migration
   def self.up
     create_table :services do |t|
-      t.string :name
       t.references :category
-      t.integer :start_price
-      t.integer :end_price
+      t.string :name
+      t.integer :beginner_start_price
+      t.integer :beginner_end_price
+      t.integer :intermidiate_start_price
+      t.integer :intermidiate_end_price
+      t.integer :senior_start_price
+      t.integer :senior_end_price
       t.timestamps
     end
   end
