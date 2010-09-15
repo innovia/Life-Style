d98bebeb5c5e1e6e38ea96e5f588b7ab85196933
innovia <ami.mahloof@gmail.com>
2010-09-14 23:55:37 -0400

    Products as one page + navs; Stylists Close button YUI disable on logged off users

--- db/migrate/20100914211425_add_desc_to_product.rb
+++
@@ -1,9 +0,0 @@
-class AddDescToProduct < ActiveRecord::Migration
-  def self.up
-    add_column :products, :desc, :string
-  end
-
-  def self.down
-    remove_column :products, :desc, :string
-  end
-end
