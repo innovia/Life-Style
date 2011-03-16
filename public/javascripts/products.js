45a7d87a281a9f2068646962df10b11f20db5357
Ami <ami@it18.skb.med.nyu.edu>
2011-03-16 14:48:24 -0400

    jquery tooltip for editing a product

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
+	$(".tooltip-trigger").tooltip({ effect: 'slide', position: 'center left', delay: 500});
+		
 	$('.product-title').bind('click', function(event) {
  		var product = $(this).attr("data-productID");
-		
 		$.get('/products/' + product,function(data){
 			$('#product-focus').html(data)
 		});
