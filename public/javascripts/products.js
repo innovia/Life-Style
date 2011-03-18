42aa860f8657d8c1f66d19f5637d4e61be4f94bd
Ami <ami@it18.skb.med.nyu.edu>
2011-03-18 12:12:26 -0400

    Products V2 Ready

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {		
 	
 	$('.product-title').bind('click', function(event) {
-		$('.product-title').removeClass("highlight");
-		$(this).toggleClass("highlight");
+		$('.product-title').removeClass("selected-product");
+		$(this).toggleClass("selected-product");
  		var product = $(this).attr("data-productID");
 		$.get('/products/' + product,function(data){
 			$('#product-focus').html(data)
