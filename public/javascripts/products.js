fed9a15a522927b1c3c8d179ad4e7c7b1f48a4a2
Ami <ami@it18.skb.med.nyu.edu>
2011-03-18 12:23:59 -0400

    Product reflections added

--- public/javascripts/products.js
+++ public/javascripts/products.js
@@ -1,11 +1,12 @@
 $(document).ready(function() {		
-	
+		
 	$('.product-title').bind('click', function(event) {
 		$('.product-title').removeClass("selected-product");
 		$(this).toggleClass("selected-product");
  		var product = $(this).attr("data-productID");
 		$.get('/products/' + product,function(data){
-			$('#product-focus').html(data)
+			$('#product-focus').html(data);
+				$('.reflect').reflect({height: 0.33, opacity: 0.7});
 		});
 	});
 	
